8bad6f6eae5d83841db62adfb3295292deca94d6
Xicano619 <ivan.sillasnavarro@gmail.com>
2020-12-03 01:03:20 -0800

    Committed updated readme file

---
+++ index.js
@@ -0,0 +1,80 @@
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your projects name?',
+        name: 'title'
+
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description'
+
+    },
+    {
+        type: 'input',
+        message: 'What kind of license should your project have?',
+        name: 'licence'
+
+    },
+    {
+        type: 'input',
+        message: 'What are your installations instructions?',
+        name: 'installation'
+
+    },
+    {
+        type: 'input',
+        message: 'What is your application used for?',
+        name: 'usage'
+
+    },
+    {
+        type: 'input',
+        message: 'Do you have external contributing developers? If so, please input their information.',
+        name: 'credits'
+
+    },
+    {
+        type: 'input',
+        message: 'Do you have testing commands/instructions?',
+        name: 'tests'
+
+    },
+    {
+        type: 'input',
+        message: 'Whats your email address?',
+        name: 'email'
+
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github'
+
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err): console.log('Success!');
+    })
+}
+
+// function to initialize program
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+    writeToFile('ReadME.md', generateMarkdown(data))
+    })
+}
+
+// function call to initialize program
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,10 @@
-# Generate ReadMe
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
 
 ## ![badges](https://img.shields.io/badge/license-mit-green)
 ## Description 
-Create an server side application for clients so that they spend more time on project.
+${data.description}
 
 ## Table of Contents (Optional):
 Add a table of contents if your README is very long that way the user finds what they need.
@@ -13,31 +15,35 @@ Add a table of contents if your README is very long that way the user finds what
 * [License](#license)
 
 ## Installation
-this application requires the dependencies: Inquire. 
+${data.installation} 
 What are your installations instructions? 
 
 ## Usage 
-this application requires the dependencies: Inquire. 
+${data.installation} 
 Using screenshots, show examples for use and lay out instructions.
 To install necessary dependencies, run the following command:
 npm i
 ## Credits
-not yet.  
+${data.credits}  
 Collaborators (links):
 
-Github profiles: xicano619 
+Github profiles: ${data.github} 
 
 LinkedIn: 
 
-Email: Ivan.sillasnavarro@gmail.com 
+Email: ${data.email} 
 
 ## License
-undefined 
+${data.license} 
 Protect your intellectual property by letting other developers know what they CAN and CANNOT do with your project. 
 Need help choosing a license, check out: [https://choosealicense.com/](https://choosealicense.com/)
 
 ---
 ## Tests 
 Provide the test for your application and show examples on how to run them.
-working on it.
+${data.tests}
 
+`;
+}
+
+module.exports = generateMarkdown;
